7e78f2d6a46b0a07d87723b5a602e88c2341cb4f
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-09-27 05:11:52 +0530

    Add Pages

--- Rajarata Pawning Centre/routes/web.php
+++ Rajarata Pawning Centre/routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+// Route for Services Page
+Route::get('/services', function () {
+    return view('services'); // Returns the services.blade.php view
+})->name('services');
+
+// Route for About Page
+Route::get('/about', function () {
+    return view('about'); // Returns the about.blade.php view
+})->name('about');
+
+// Route for Contact Page
+Route::get('/contact', function () {
+    return view('contact'); // Returns the contact.blade.php view
+})->name('contact');
