27532b6491e6b73cd3510e531d92eceb5808a36d
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-10-03 00:32:24 +0530

    Add Languages Change Part

---
+++ Rajarata Pawning Centre/resources/lang/si/messages.php
@@ -0,0 +1,41 @@
+<?php
+return [
+
+    /* Services page */
+    'Serv-title' => 'අපගේ සේවාවන්',
+    'Serv-description' => 'ඔබගේ අවශ්‍යතා සපුරාලීම සඳහා අපි උකස් සේවා රැසක් පිරිනමන්නෙමු. පහත විස්තර ඇත:',
+    'Serv-max_loan' => 'උපරිම ණය මුදල',
+    'Serv-interest_rate' => 'පොලී අනුපාතය',
+    'Serv-interest_rate_desc' => 'මසකට 1% පොලියක්',
+    'Serv-min_pawning' => 'අවම උකස් ප්‍රමාණය',
+    'Serv-min_pawning_desc' => 'රන් 200 mg',
+    'Serv-grace_period' => 'පොලී රහිත සහන කාලය',
+    'Serv-grace_period_desc' => 'පළමු මාසය (විශේෂිත ණය වර්ග සඳහා)',
+    'Serv-working_hours' => 'වැඩ කරන වේලාවන්',
+    'Serv-working_hours_desc' => 'උදේ 8:00 සිට සවස 5:00 දක්වා',
+    'Serv-redemption' => 'ප්‍රතිලාභ සහ අලුත් කිරීම',
+    'Serv-redemption_desc' => 'මාසයක් ඇතුළත',
+    'Serv-payment_plans' => 'නම්‍යශීලී ගෙවීම් සැලසුම්',
+    'Serv-payment_plans_desc' => 'ඔබගේ අවශ්යතා මත පදනම්ව',
+    'Serv-customer_benefits' => 'නිත්‍ය පාරිභෝගිකයන් සඳහා විශේෂ ප්‍රතිලාභ',
+    'Serv-customer_benefits_desc' => 'විශේෂ වාසියන් ලබා ගත හැක',
+    'Serv-contact_info' => 'වැඩි විස්තර සඳහා, අප අමතන්න:',
+
+
+    /* About page */
+    'About-title' => 'අපි ගැන',
+    'About-description' => 'උකස් මධ්‍යස්ථානය පිළිබඳ තොරතුරු...',
+
+
+    /* Footer */
+    'Foot-title' => 'රජරට උකස් මධ්‍යස්ථානය',
+    'Foot-mail' => 'විද්‍යුත් ලිපිනය',
+    'Foot-address_1' => 'සිරිපුර නව නගරය',
+    'Foot-address_2' => 'පොලොන්නරුව, ශ්‍රී ලංකාව',
+    'Foot-pages' => 'පිටුව',
+    'Foot-page_1' => 'මුල් පිටුව',
+    'Foot-page_2' => 'ගැන',
+    'Foot-page_3' => 'සේවාව',
+    'Foot-page_4' => 'අමතන්න',
+
+];
